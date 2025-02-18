9ea82f2cd3d3d8e17911bff7bfb31c40cf1efbfc
Tim Morgan <tim@timmorgan.org>
2025-02-18 12:51:47 -0600

    Prefix redis keys for easier identification
    Now the keys look like this:
    
    - clutch:db694a2e-64cf-40be-b2c8-ae94e8a77038:jobs
    - clutch:db694a2e-64cf-40be-b2c8-ae94e8a77038:result-1
    - clutch:db694a2e-64cf-40be-b2c8-ae94e8a77038:result-2

--- lib/sidekiq/clutch.rb
+++ lib/sidekiq/clutch.rb
@@ -89,7 +89,7 @@ module Sidekiq
       service = self.class.new(parent_batch)
       service.key_base = @key_base
       service.jobs.raw = remaining_jobs
-      service.current_result_key = "#{key_base}-#{options['result_key_index']}"
+      service.current_result_key = "#{key_base}:result-#{options['result_key_index']}"
       service.engage
     end
 
@@ -116,11 +116,11 @@ module Sidekiq
     end
 
     def key_base
-      @key_base ||= SecureRandom.uuid
+      @key_base ||= "clutch:#{SecureRandom.uuid}"
     end
 
     def jobs_key
-      "#{key_base}-jobs"
+      "#{key_base}:jobs"
     end
 
     def set_jobs_data_in_redis(data)
@@ -155,7 +155,7 @@ module Sidekiq
 
     def enqueue_job(klass, params, result_key_index)
       job_options = Object.const_get(klass).sidekiq_options
-      result_key = "#{key_base}-#{result_key_index}"
+      result_key = "#{key_base}:result-#{result_key_index}"
       options = {
         'class'     => JobWrapper,
         'queue'     => queue || job_options['queue'],
@@ -172,7 +172,7 @@ module Sidekiq
         redis.del(jobs_key)
         result_key_index = 1
         loop do
-          result = redis.del("#{key_base}-#{result_key_index}")
+          result = redis.del("#{key_base}:result-#{result_key_index}")
           result_key_index += 1
           break if result == 0
         end
